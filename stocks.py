3a50d8419da3cc966fe33268a00cb1c38889d2cf
Ghazi Farooq <mail@ghazif.com>
2020-12-01 10:13:05 -0600

    initial commit

---
+++ stocks.py
@@ -0,0 +1,85 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import numpy as np
+from datetime import datetime
+import pandas as pd
+from tiingo import TiingoClient
+
+app = dash.Dash()
+
+nsdq = pd.read_csv('clist.csv')
+nsdq.set_index('Symbol', inplace=True)
+options = []
+for tic in nsdq.index:
+    options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
+
+
+app.layout = html.Div([
+    html.H1('Stock Ticker Dashboard'),
+    html.Div([
+        html.H3('Select stock symbols:', style={'paddingRight':'30px'}),
+        dcc.Dropdown(
+            id='my_ticker_symbol',
+            options=options,
+            value=['TSLA'],
+            multi=True
+        )
+    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
+    html.Div([
+        html.H3('Select start and end dates:'),
+        dcc.DatePickerRange(
+            id='my_date_picker',
+            min_date_allowed=datetime(2015, 1, 1),
+            max_date_allowed=datetime.today(),
+            start_date=datetime(2018, 1, 1),
+            end_date=datetime.today()
+        )
+    ], style={'display':'inline-block'}),
+    html.Div([
+        html.Button(
+            id='submit-button',
+            n_clicks=0,
+            children='Update',
+            style={'fontSize':24, 'marginLeft':'30px'}
+        ),
+    ], style={'display':'inline-block'}),
+    dcc.Graph(
+        id='my_graph',
+        figure={
+            'data': [
+                {'x': [1,2], 'y': [3,1]}
+            ]
+        }
+    )
+])
+@app.callback(
+    Output('my_graph', 'figure'),
+    [Input('submit-button', 'n_clicks')],
+    [State('my_ticker_symbol', 'value'),
+    State('my_date_picker', 'start_date'),
+    State('my_date_picker', 'end_date')])
+def update_graph(n_clicks, stock_ticker, start_date, end_date):
+    start = datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    traces = []
+
+    config = {}
+    # To reuse the same HTTP Session across API calls (and have better performance), ˓→include a session key.
+    config['api_key'] = '94bc55bd22b602be267b1449201ce793e96fa607'
+    # If you don't have your API key as an environment variable, # pass it in via a configuration dictionary. config['api_key'] = "MY_SECRET_API_KEY"
+    # Initialize
+    client = TiingoClient(config)
+
+    for tic in stock_ticker:
+        df = client.get_dataframe(tic, start, end)
+        traces.append({'x':df.index, 'y': df.close, 'name':tic})
+    fig = {
+        'data': traces,
+        'layout': {'title':', '.join(stock_ticker)+' Closing Prices'}
+    }
+    return fig
+
+if __name__ == '__main__':
+    app.run_server()
